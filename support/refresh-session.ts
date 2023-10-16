79f05f40a888d836cf4b50b340992c8ebb726c8d
Jacob Gillespie <jacobwgillespie@gmail.com>
2023-10-16 16:43:55 +0100

    Add ADMIN_TOKEN

--- support/refresh-session.ts
+++ support/refresh-session.ts
@@ -16,43 +16,48 @@ declare global {
 async function main() {
   const browser = await chromium.launch({headless: !Boolean(process.env.LOCAL)})
   const page = await browser.newPage()
-  await page.goto('https://github.com/')
-  await page.getByRole('link', {name: 'Sign in'}).click()
-  await page.getByLabel('Username or email address').click()
-  await page.getByLabel('Username or email address').fill(process.env.USERNAME)
-  await page.getByLabel('Password').click()
-  await page.getByLabel('Password').fill(process.env.PASSWORD)
-  await page.getByRole('button', {name: 'Sign in', exact: true}).click()
-  await page.getByPlaceholder('XXXXXX').click()
-  await page.getByPlaceholder('XXXXXX').fill(process.env.OTP)
-  await Promise.race([page.getByLabel('Open user account menu').click(), page.getByText('Confirm').click()])
-  await page.getByRole('link', {name: 'Your profile'}).click()
-  await page.getByRole('link', {name: process.env.USERNAME}).click()
-  const cookies = await page.context().cookies()
-  const sessionCookie = cookies.find((c) => c.name === 'user_session')
 
-  if (!sessionCookie) throw new Error('no session cookie')
+  try {
+    await page.goto('https://github.com/')
+    await page.getByRole('link', {name: 'Sign in'}).click()
+    await page.getByLabel('Username or email address').click()
+    await page.getByLabel('Username or email address').fill(process.env.USERNAME)
+    await page.getByLabel('Password').click()
+    await page.getByLabel('Password').fill(process.env.PASSWORD)
+    await page.getByRole('button', {name: 'Sign in', exact: true}).click()
+    await page.getByPlaceholder('XXXXXX').click()
+    await page.getByPlaceholder('XXXXXX').fill(process.env.OTP)
+    await Promise.race([page.getByLabel('Open user account menu').click(), page.getByText('Confirm').click()])
+    await page.getByRole('link', {name: 'Your profile'}).click()
+    await page.getByRole('link', {name: process.env.USERNAME}).click()
+    const cookies = await page.context().cookies()
+    const sessionCookie = cookies.find((c) => c.name === 'user_session')
 
-  await page.screenshot({path: 'tmp/screenshot.png'})
+    if (!sessionCookie) throw new Error('no session cookie')
 
-  if (process.env.LOCAL) {
-    console.log(sessionCookie)
+    await page.screenshot({path: 'tmp/screenshot.png'})
+
+    if (process.env.LOCAL) {
+      console.log(sessionCookie)
+    }
+
+    if (process.env.ADMIN_TOKEN) {
+      console.log('Updating GitHub session...')
+      await fetch('https://actions-public-oidc.depot.dev/-/github-session', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${process.env.ADMIN_TOKEN}`,
+        },
+        body: JSON.stringify({session: sessionCookie.value}),
+      })
+    } else {
+      throw new Error('no ADMIN_TOKEN')
+    }
+  } finally {
+    await page.close()
+    await browser.close()
   }
-
-  if (process.env.ADMIN_TOKEN) {
-    console.log('Updating GitHub session...')
-    await fetch('https://actions-public-oidc.depot.dev/-/github-session', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: `Bearer ${process.env.ADMIN_TOKEN}`,
-      },
-      body: JSON.stringify({session: sessionCookie.value}),
-    })
-  }
-
-  await page.close()
-  await browser.close()
 }
 
 main().catch((err) => {
